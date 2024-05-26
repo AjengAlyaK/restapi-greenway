80bd0055146bcc4bffee1fc7bc1c9b8cbeec76ff
Ria <songriah98@gmail.com>
2024-05-26 17:51:12 +0700

    add value job

--- controllers/review.mjs
+++ controllers/review.mjs
@@ -16,16 +16,17 @@ const auth = getAuth(fireInit);
 const db = getFirestore(fireInit);
 
 export const review = async (req, res) => {
-    const { name, review, photo } = req.body;
+    const { name, review, photo, job } = req.body;
     // Validate required fields
-    if (!name || !review || !photo) {
-        return res.status(400).json({ error: "name, review, and photo are required." });
+    if (!name || !review || !photo || !job) {
+        return res.status(400).json({ error: "name, review, photo, job are required." });
     }
     try {
         const addReview = await addDoc(collection(db, "reviews"), {
             name: name,
             review: review,
-            photo: photo
+            photo: photo,
+            job: job
         });
         return res.status(200).json({
             status: "success",
@@ -35,7 +36,8 @@ export const review = async (req, res) => {
                     id: addReview.id,
                     name,
                     review,
-                    photo
+                    photo,
+                    job
                 }
             }
         });
